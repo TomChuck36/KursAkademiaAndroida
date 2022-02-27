3138d8ca4e8592d96289900aeafd4c6347595ef8
Adrian Tomczak <lbpro.adrian.tomczak@gmail.com>
2022-02-27 18:40:40 +0100

    Added saveEpisodes to dao and repository

--- app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/data/local/EpisodeDao.kt
+++ app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/data/local/EpisodeDao.kt
@@ -1,8 +1,13 @@
 package com.atomczak.kursakademiaandroida.features.episodes.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.atomczak.kursakademiaandroida.features.episodes.data.local.model.EpisodeCached
 
 @Dao
 interface EpisodeDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveEpisodes(vararg episode: EpisodeCached)
 }
--- app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/data/repository/EpisodeRepositoryImpl.kt
+++ app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/data/repository/EpisodeRepositoryImpl.kt
@@ -1,16 +1,29 @@
 package com.atomczak.kursakademiaandroida.features.episodes.data.repository
 
 import com.atomczak.kursakademiaandroida.core.api.RickAndMortyApi
+import com.atomczak.kursakademiaandroida.features.episodes.data.local.EpisodeDao
+import com.atomczak.kursakademiaandroida.features.episodes.data.local.model.EpisodeCached
 import com.atomczak.kursakademiaandroida.features.episodes.domain.EpisodeRepository
 import com.atomczak.kursakademiaandroida.features.episodes.domain.model.Episode
 
 class EpisodeRepositoryImpl(
-    private val api: RickAndMortyApi
+    private val rickAndMortyApi: RickAndMortyApi,
+    private val episodeDao: EpisodeDao
 ) : EpisodeRepository {
 
     override suspend fun getEpisodes(): List<Episode> {
-        return api.getEpisodes().results.map {
+        return rickAndMortyApi.getEpisodes().results.map {
             it.toEpisode()
+        }.also {
+            saveEpisodes(it)
+        }
+    }
+
+    private suspend fun saveEpisodes(episodes: List<Episode>) {
+        episodes.map {
+            EpisodeCached(it)
+        }.toTypedArray().let {
+            episodeDao.saveEpisodes(*it)    // * służy do mapowania TypedArray na vararg
         }
     }
 }
