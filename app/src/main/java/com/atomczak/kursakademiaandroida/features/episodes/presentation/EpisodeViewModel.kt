7e2bc8ee0fdfd411fc1889bf696f492c41085d13
Adrian Tomczak <lbpro.adrian.tomczak@gmail.com>
2022-03-02 11:55:45 +0100

    Added ViewModel for episode

---
+++ app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/presentation/EpisodeViewModel.kt
@@ -0,0 +1,34 @@
+package com.atomczak.kursakademiaandroida.features.episodes.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.atomczak.kursakademiaandroida.features.episodes.domain.GetEpisodesUseCase
+import com.atomczak.kursakademiaandroida.features.episodes.presentation.model.EpisodeDisplayable
+
+class EpisodeViewModel(
+    private val getEpisodesUseCase: GetEpisodesUseCase
+) : ViewModel() {
+
+    val episodes by lazy {
+        MutableLiveData<List<EpisodeDisplayable>>().also {
+            getEpisodes(it)
+        }
+    }
+
+    private fun getEpisodes(episodeLiveData: MutableLiveData<List<EpisodeDisplayable>>) {
+        getEpisodesUseCase(
+            params = Unit,
+            scope = viewModelScope
+        ) { result ->
+            result.onSuccess { episodes ->
+                episodeLiveData.value = episodes.map {
+                    EpisodeDisplayable(it)
+                }
+            }
+            result.onFailure {
+
+            }
+        }
+    }
+}
