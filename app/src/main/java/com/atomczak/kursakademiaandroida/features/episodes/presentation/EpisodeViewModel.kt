62c457435878ca7b0907455a88cb7b46b28aae8b
Adrian Tomczak <lbpro.adrian.tomczak@gmail.com>
2022-03-09 22:11:42 +0100

    Created generic BaseViewModel

--- app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/presentation/EpisodeViewModel.kt
+++ app/src/main/java/com/atomczak/kursakademiaandroida/features/episodes/presentation/EpisodeViewModel.kt
@@ -1,21 +1,17 @@
 package com.atomczak.kursakademiaandroida.features.episodes.presentation
 
-import androidx.lifecycle.*
-import com.atomczak.kursakademiaandroida.core.base.UiState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
+import com.atomczak.kursakademiaandroida.core.base.BaseViewModel
 import com.atomczak.kursakademiaandroida.features.episodes.domain.GetEpisodesUseCase
 import com.atomczak.kursakademiaandroida.features.episodes.domain.model.Episode
 import com.atomczak.kursakademiaandroida.features.episodes.presentation.model.EpisodeDisplayable
-import com.hadilq.liveevent.LiveEvent
 
 class EpisodeViewModel(
     private val getEpisodesUseCase: GetEpisodesUseCase
-) : ViewModel() {
-
-    private val _message by lazy { LiveEvent<String>() }
-    val message: LiveData<String> by lazy { _message }
-
-    private val _uiState by lazy { MutableLiveData<UiState>() }
-    val uiState: LiveData<UiState> by lazy { _uiState }
+) : BaseViewModel() {
 
     private val _episodes by lazy {
         MutableLiveData<List<Episode>>().also {
@@ -38,26 +34,8 @@ class EpisodeViewModel(
             scope = viewModelScope
         ) { result ->
             setIdleState()
-            result.onSuccess { episodes ->
-                episodeLiveData.value = episodes
-            }
-            result.onFailure { throwable ->
-                throwable.message?.let {
-                    showMessage(it)
-                }
-            }
+            result.onSuccess { episodeLiveData.value = it }
+            result.onFailure { handleFailure(it) }
         }
-    }
-
-    private fun showMessage(message: String) {
-        _message.value = message
-    }
-
-    private fun setIdleState() {
-        _uiState.value = UiState.Idle
-    }
-
-    private fun setPendingState() {
-        _uiState.value = UiState.Pending
     }
 }
