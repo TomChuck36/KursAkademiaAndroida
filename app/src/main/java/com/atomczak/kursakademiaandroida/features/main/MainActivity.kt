0f4424e3b1454c66e7c2fe0a682d0fdc8a4be1a2
Adrian Tomczak <lbpro.adrian.tomczak@gmail.com>
2022-06-12 09:46:49 +0200

    Init Navigation Component

--- app/src/main/java/com/atomczak/kursakademiaandroida/features/main/MainActivity.kt
+++ app/src/main/java/com/atomczak/kursakademiaandroida/features/main/MainActivity.kt
@@ -2,11 +2,33 @@ package com.atomczak.kursakademiaandroida.features.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.atomczak.kursakademiaandroida.R
+import com.atomczak.kursakademiaandroida.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_characters, R.id.navigation_episodes, R.id.navigation_locations)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
